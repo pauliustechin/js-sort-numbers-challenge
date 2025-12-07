4bb8c662ec40742b17e7c0b86934ac0ad9045cba
pauliustechin <pauliustechin@gmail.com>
2025-12-07 17:45:08 +0200

    finish implementation of an array asc order logic

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ import { SortNumbers } from './sort-numbers.js';
 
 "use strict";
 
-const numArray = [1, 5, 3, 4, 9];
+const numArray = [2, 5, 3, 4, 3, 2, 2, 1, 3, 9, 8, 6];
 const num = 1;
 
 const sortNums = new SortNumbers(numArray);
 
 sortNums.sortSmall("desc");
-
-console.log(numArray.splice(1,2));
--- sort-numbers.js
+++ sort-numbers.js
@@ -51,6 +51,73 @@ export class SortNumbers{
       }
     }
 
+    #arrayLengthMoreThanTwoAsc(){
+
+      // kadangi zinau, kad array length > 2, pradedu counta nuo 2.
+      let finalArrayAsc = this.#arrayLengthIsTwo("asc");
+      const arrayLength = this.myArray.length;
+      let count = 2;
+      
+      let currentNumber;
+      let tempArray;
+
+
+      // sukti while loop, kol bus praeiti visi array skaiciai.
+      while(count !== arrayLength){
+
+        // susikuriau internal count, kad galeciau tikrinti, kuris skaicius surikiuotos array tikrinamas.
+        let internalCount = count - 1;
+        currentNumber = this.myArray[count];
+        // final length, kad zinoti kiek skaiciu isimti is atnaujintos array.
+        const finalLength = finalArrayAsc.length;
+
+        // leidziam for loop per atnaujinta array.
+        for(let i = 0; i < count; i++){
+
+          // paskutinis newArray skaicius mazesnis, uz tikrinama skaiciu, skaiciu is kart pridedam i pabaiga.
+          if(currentNumber >= finalArrayAsc[count - 1]){
+            finalArrayAsc.push(currentNumber);
+            break;
+          }
+          // jei praeina visas ciklas ir neatsiranda nei vienas mazesnis skaicius, tikrinama skaiciu dedam i prieki.
+          else if(internalCount === 0){
+            // tempArray = [];
+            finalArrayAsc.unshift(currentNumber);
+            break;
+          }
+          
+          // jei paskutinis tikrintas skaicius didesnis uz currentNumber, o ankstesnis eileje didesnis,
+          // nupjaunam visus didesnius skaicius i tempArray (uodega veliau pridesim prie final array)
+          else if((currentNumber <= finalArrayAsc[internalCount]) && 
+                  (currentNumber >= finalArrayAsc[internalCount-1])){
+            tempArray = finalArrayAsc.splice(internalCount);
+
+          // istrinam visa uodega > skaicius didesnius uz current number,
+          // o current number pushinam i paskutine vieta.
+            for(let i = internalCount; i < finalLength; i++){
+              finalArrayAsc.splice(i, 1);
+            }
+
+            finalArrayAsc.push(currentNumber);
+          
+          // prie modifikuoto final array pridedam uodega.
+            tempArray.forEach((num) => {
+              finalArrayAsc.push(num);
+            })
+            break;
+          }
+
+          // jei netenkina nei vienos salygos, internal count minusuojam ir ziurim i ankstesni skaiciu masyve.
+          else{
+            internalCount--;
+          }
+        }
+        count++;
+      }
+      return finalArrayAsc;
+    }
+
+
 
 
     sortSmall(order){
@@ -66,6 +133,9 @@ export class SortNumbers{
       else if(this.myArrayValid.checkLength() === 2){
         newArray = this.#arrayLengthIsTwo(order);
         console.log(newArray);
+      }
+      else{
+        this.#arrayLengthMoreThanTwoAsc();
       }
     }
 }
